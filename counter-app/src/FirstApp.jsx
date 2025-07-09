abb9cbb44adeb305f76fd8910fa46b5357dbe586
RmzGerardo <lalitomendez132@gmail.com>
2025-07-09 12:39:39 -0600

    Subida de curso de react

---
+++ counter-app/src/FirstApp.jsx
@@ -0,0 +1,26 @@
+
+
+export function Examen () {
+    return (
+        <>
+        <h1>Examen</h1>
+        <h2>Una funcion para retornar algo y hacerlo mas dinamico</h2>
+        <h3>Curso de React</h3>
+        <h4>Desarrollador de Software</h4>
+        </>
+    )
+}
+
+
+export const FirstApp = () => {
+
+
+    return (
+        <>
+
+           { Examen()}
+            {/* <code> { JSON.stringify(newMessage) } </code> */}
+            <p>Pasante</p>
+        </>
+    )
+}
