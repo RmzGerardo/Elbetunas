c55dbd16d5a8062a2f8b60824551eacccb49a4e6
RmzGerardo <lalitomendez132@gmail.com>
2025-07-27 00:52:58 -0600

    Cambios de diseño

--- src/main.jsx
+++ src/main.jsx
@@ -12,13 +12,10 @@ import "./portafolio/about.css";
 // global
 import "./app.css";
 
-import { Life } from "./about_main/life.jsx";
-import "./about_main/life.css";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
     <About />
-    <Life />
+    {/* <Life /> */}
   </StrictMode>
 );
