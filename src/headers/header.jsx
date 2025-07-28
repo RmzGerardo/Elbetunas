00144cdc035f857047f2099ce7bc519e11b557d2
RmzGerardo <lalitomendez132@gmail.com>
2025-07-28 12:24:51 -0600

    Agregar nuevo mensaje en el componente Typewriter del encabezado

--- src/headers/header.jsx
+++ src/headers/header.jsx
@@ -113,7 +113,12 @@ export const Header = () => {
           <div className="col-auto d-flex flex-column justify-content-center align-items-center">
             <h1 className="text-center titulo-animado">
               <Typewriter
-                words={["Elbetunas", "Bienvenido@ Dev", "Portafolio React"]}
+                words={[
+                  "Elbetunas",
+                  "Bienvenido@ Dev",
+                  "Portafolio React",
+                  "Desarrollador Software",
+                ]}
                 loop={0}
                 cursor
                 cursorStyle="_"
