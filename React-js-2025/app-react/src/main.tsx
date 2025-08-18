6a9d52459fdfb977072cb08ab1b38108f301477a
RmzGerardo <lalitomendez132@gmail.com>
2025-08-18 13:08:16 -0600

    feat: Refactor application structure by removing unused components and styles, and adding new AppAwesome and ItemCounter components

--- React-js-2025/app-react/src/main.tsx
+++ React-js-2025/app-react/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Primer } from './Primer';
+import { ItemCounter } from './assets/carrito/ItemCounter';
+import { AppAwesome } from './AppAwesome';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppAwesome/>
   </StrictMode>,
 )
