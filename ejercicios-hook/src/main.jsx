39f0a3bf89f97edb7f7c73cb6d26acc62280011a
RmzGerardo <lalitomendez132@gmail.com>
2025-08-18 21:19:31 -0600

    sd

--- ejercicios-hook/src/main.jsx
+++ ejercicios-hook/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { HookApp } from "./HookApp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HookApp />
   </StrictMode>
 );
